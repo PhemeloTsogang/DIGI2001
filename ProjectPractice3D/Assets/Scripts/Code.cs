2aaf911ab4381f70a1df9956a9edd89de029fffc
PhemeloTsogang <2543064@students.wits.ac.za>
2025-07-24 15:34:24 +0200

    set up Project file structure

---
+++ ProjectPractice3D/Assets/Scripts/Code.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Code : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("sup Phem");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
